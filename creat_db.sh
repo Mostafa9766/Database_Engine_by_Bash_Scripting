f03d6a70f5b2d347530d6f42e99bbfeb628e0089
omar Fathy <omarmfathy219@gmail.com>
2022-09-01 09:29:17 +0200

    fix regex condition

--- creat_db.sh
+++ creat_db.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 source db_colours.sh #all  ANSI code color and font format stored in db_colours.sh
-#regex='[+*.-_{}$><|:;@-\^(/)=^#!~&`"]';
-#regex='[+*-/]';
+regex='[+*.-_{}$><|:;@-\^(/)=^#!~&`"$]';
 
 
 echo -e "${yellow}---------Create Database--------${none}"
 read -p "Enter Database Name: " dbname 
-while [[ -z $dbname ]] || [[ $dbname =~ $regex ]] || [[ $dbname =~ [0-9] ]]
-do 
+if [[ -z $dbname ]] || [[ $dbname =~ $regex ]] || [[ $dbname =~ [0-9] ]]
+then 
 	echo -e "${red}Invalid Database Name... ${none}"
 	read -p "PLease Enter Database Name Again: " dbname
-done
-
-if [ -d ./Databases/$dbname ] 
+	exit
+elif	
+[ -d ./Databases/$dbname ] 
 then
     echo -e "${red}Database already exists -_- ${none}"
-   
+elif
+[ ! -d ./Databases/$dbname ]
+then
+	mkdir ./Databases/$dbname
+	echo -e "${green}Database created successfully ^_^ ${none}"
 fi
-mkdir ./Databases/$dname  
-echo -e "${yellow} Database *>> $dbname <<*  was Created -_*${none}"	
-
-
-
-
-
